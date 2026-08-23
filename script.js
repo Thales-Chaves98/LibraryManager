6f83937e9127b69a8e70961d4b89d68d1d44d1a5
Thales <thales.augusto.chaves98@gmail.com>
2026-08-23 11:36:18 -0300

    create render book

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const addBookModal = document.getElementById("add-book-modal");
 const cancelAddBook = document.getElementById("cancel-btn");
 const saveAddBook = document.getElementById("save-btn");
 
+//Book Container
+const bookContainer = document.getElementById("books-container");
 
 let nextBookId = 1;
 const books = [];
@@ -31,6 +33,7 @@ saveAddBook.addEventListener('click', () =>{
     
     
     closeModal(addBookModal);
+    renderBooks();
 });
 
 function createBookObject(){
@@ -56,7 +59,75 @@ function createBookObject(){
 }
 
 function renderBooks(){
+    bookContainer.innerHTML = '';
 
+    books.forEach(book => {
+
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+
+        const bookDescription = document.createElement("div");
+        bookDescription.classList.add("book-description");
+
+        const bookTitle = document.createElement("p");
+        bookTitle.classList.add("book-title");
+        bookTitle.textContent = book.title;
+
+        const bookAuthor = document.createElement("p");
+        bookAuthor.classList.add("book-author");
+        bookAuthor.textContent = book.author;
+
+
+        const bookActions = document.createElement("div");
+        bookActions.classList.add("book-actions");
+
+        const readBtn = document.createElement("button");
+        readBtn.classList.add("read-btn");
+
+        const readIcon = document.createElement("span");
+        readIcon.classList.add("material-symbols-outlined");
+        readIcon.textContent = "bookmark";
+        
+        const favoriteBtn = document.createElement("button");
+        favoriteBtn.classList.add("favorite-btn");
+
+        const favoriteIcon = document.createElement("span");
+        favoriteIcon.classList.add("material-symbols-outlined");
+        favoriteIcon.textContent = "favorite";
+
+        const editBtn = document.createElement("button");
+        editBtn.classList.add("edit-btn");
+
+        const editIcon = document.createElement("span");
+        editIcon.classList.add("material-symbols-outlined");
+        editIcon.textContent = "edit";
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("delete-btn");
+
+        const deleteIcon = document.createElement("span");
+        deleteIcon.classList.add("material-symbols-outlined");
+        deleteIcon.textContent = "delete";
+
+        readBtn.append(readIcon);
+        favoriteBtn.append(favoriteIcon);
+        editBtn.append(editIcon);
+        deleteBtn.append(deleteIcon);
+
+        bookActions.append(
+            readBtn, favoriteBtn, editBtn, deleteBtn
+        );
+
+        bookDescription.append(
+            bookTitle, bookAuthor
+        );
+        
+        bookCard.append(
+            bookDescription, bookActions
+        );
+
+        bookContainer.append(bookCard);
+    });
 }
 
 function openModal(modal){

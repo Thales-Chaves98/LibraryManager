103ef5eb4deaa829b57af8e04603f74099fc4654
Thales <thales.augusto.chaves98@gmail.com>
2026-08-22 21:57:35 -0300

    starting javascript, close and open modal , object creation

---
+++ script.js
@@ -0,0 +1,55 @@
+const addBookBtn = document.getElementById("add-book-btn");
+
+//Form
+let inputBookTitle = document.getElementById("book-title");
+let inputBookAuthor = document.getElementById("book-author");
+
+
+//Add Book Modal
+const addBookModal = document.getElementById("add-book-modal");
+const cancelAddBook = document.getElementById("cancel-btn");
+const saveAddBook = document.getElementById("save-btn");
+
+
+let nextBookId = 0;
+const books = [];
+
+addBookBtn.addEventListener('click', (event) =>{
+    event.preventDefault();
+
+    openModal(addBookModal);
+
+
+});
+
+cancelAddBook.addEventListener('click', () =>{
+    closeModal(addBookModal);
+});
+saveAddBook.addEventListener('click', () =>{
+    
+    createBookObject();
+    
+    
+    closeModal(addBookModal);
+});
+
+function createBookObject(){
+
+    const title = inputBookTitle.value.trim().toUpperCase(); 
+    const author = inputBookAuthor.value.trim().toUpperCase(); 
+
+    const book = {
+        id: nextBookId,
+        title,
+        author
+    };
+
+    console.log(book);
+}
+
+function openModal(modal){
+    modal.classList.add("show");
+}
+function closeModal(modal){
+    modal.classList.remove("show");
+}
